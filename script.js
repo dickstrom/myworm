e60fbe630343388a7679cdea1d1755016ae85379
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-06-01 21:38:59 +0200

    Changes to program site
    Just a different video and text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    document.getElementById("moon").innerHTML = "The big rocket from NASA starts its journey to outer space";
-  }
-
+    document.getElementById("moon").innerHTML = "Actually, Yes. If you are looking for the most concentrated course of all. This is a beginners guide.";
+    document.getElementById("js").innerHTML = "In the beginning they saw that there was a need for a language that was approachable, that you could put directly in the web page.";
+}
