af2f76f4fb335516f45ffeae5cbad3223a1f038c
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-06-01 22:25:03 +0200

    updated version
    function update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function myFunction() {
+function one() {
     document.getElementById("moon").innerHTML = "Actually, Yes. If you are looking for the most concentrated course of all. This is a beginners guide.";
+}
+function two() {
     document.getElementById("js").innerHTML = "In the beginning they saw that there was a need for a language that was approachable, that you could put directly in the web page.";
 }
