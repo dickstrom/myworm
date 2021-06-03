256c9c2ced86e676e1130f0c5bda117aba2a1308
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-06-03 23:05:54 +0200

    Text changes
    Had to do the text more natural

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function one() {
-    document.getElementById("moon").innerHTML = "Actually, Yes. If you are looking for the most concentrated course of all. This is a beginners guide.";
+    document.getElementById("moon").innerHTML = "Actually, Yes. If you are looking for a guy talking a lot about Javascript. This is a beginners guide.";
 }
 function two() {
-    document.getElementById("js").innerHTML = "In the beginning they saw that there was a need for a language that was approachable, that you could put directly in the web page.";
+    document.getElementById("js").innerHTML = "No not really, but in the beginning they saw that there was a need for a language that was approachable, that you could put directly in the web page.";
 }
 
 
