a8547fecac54ed6e6c00e9edcff28a2edacdebc6
dickstrom <83727995+dickstrom@users.noreply.github.com>
2021-06-03 22:53:14 +0200

    color change and a new feature (soon)
    I´m adding my age in dogyears

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function one() {
 function two() {
     document.getElementById("js").innerHTML = "In the beginning they saw that there was a need for a language that was approachable, that you could put directly in the web page.";
 }
+
+
+
+//my age in dogyears from codecademy
+const myAge = 52;
+let earlyYears = 2;
+earlyYears *= 10,5;
+let laterYears = myAge - 2;
+laterYears *=4;
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Dick"
+console.log(`My name is ${myName}.
+           I am ${myAge} years old in human years 
+           which is ${myAgeInDogYears} years old in dog years.`);
